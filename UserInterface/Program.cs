6db355be2f32756f5d40c34b85322719947d0bd0
TuongSieuCute <108283017+TuongSieuCute@users.noreply.github.com>
2023-11-02 21:31:47 +0700

    tao branch

--- UserInterface/Program.cs
+++ UserInterface/Program.cs
@@ -13,7 +13,7 @@ namespace UserInterface {
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form1()) ;
         }
     }
 }
