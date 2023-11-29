9690be023316fdb2c8116440dd6e6f1dd0d5a1ca
TuongSieuCute <108283017+TuongSieuCute@users.noreply.github.com>
2023-11-29 18:30:43 +0700

    fix bug nhan vien

--- UserInterface/Program.cs
+++ UserInterface/Program.cs
@@ -13,7 +13,7 @@ namespace UserInterface {
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormLogin());
+            Application.Run(new FormLogin() );
         }
     }
 }
